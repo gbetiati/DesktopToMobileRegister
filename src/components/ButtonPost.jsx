fc476e065846b32c5ad2c5a149ed1cc44bd1bf8f
gbetiati <guilherme_betiati@hotmail.com>
2024-07-05 08:32:58 -0300

    second comm

--- src/components/ButtonPost.jsx
+++ src/components/ButtonPost.jsx
@@ -1,14 +1,9 @@
-const ButtonPost = ({onClickPost}) => {
-  
-  
-    return (
-        
-        <button 
-            className="bg-sky-800 p-2 mt-6 rounded-sm"
-            onClick={onClickPost}> 
-            <a className="text text-white"> Cadastrar tudo </a>
-        </button>
-    )
-}
+const ButtonPost = ({ onClickPost }) => {
+  return (
+    <button className="bg-sky-800 p-2 mt-6 rounded-sm" onClick={onClickPost}>
+      <a className="text text-white"> Cadastrar tudo ... </a>
+    </button>
+  );
+};
 
-export default ButtonPost
+export default ButtonPost;
