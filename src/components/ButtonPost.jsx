874c58e8394ac524e11f7f782b8afea96bec4e02
gbetiati <guilherme_betiati@hotmail.com>
2024-08-19 08:07:47 -0300

    z

--- src/components/ButtonPost.jsx
+++ src/components/ButtonPost.jsx
@@ -1,7 +1,7 @@
 const ButtonPost = ({ onClickPost }) => {
   return (
     <button className="bg-gradient-to-r from-[#70AFCE] from-20% via-[#3b7b9a] via-40% to-[#70AFCE]  hover:shadow-lg p-3 mt-6 rounded-md" onClick={onClickPost}>
-      <a className="text text-white font-semibold"> Cadastrar tudo ... </a>
+      <a className="text text-white font-semibold"> Cadastrar tudogit </a>
     </button>
   );
 };
